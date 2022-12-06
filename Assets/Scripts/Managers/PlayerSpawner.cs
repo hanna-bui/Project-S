32ae3dfe2b0001bde5bcf50bd66123ef595e925d
Allyssa Poulin <poulina@uwindsor.ca>
2022-12-06 14:45:26 -0500

    character selects and spawns
    -icons and names when selecting

---
+++ Assets/Scripts/Managers/PlayerSpawner.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        GameObject player = Instantiate(GameManager.instance.currCharacter.prefab, transform.position, transform.rotation);
+        player.transform.localScale = new Vector3(20, 20, 0);
+    }
+}
