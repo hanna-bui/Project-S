01d381bac65d85c2df56453da6b912a2991aac2d
Allyssa Poulin <40319407+allyssap@users.noreply.github.com>
2022-12-30 18:19:35 -0500

    item added-new branch

---
+++ Assets/Scripts/Items/Items.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Items : MonoBehaviour
+{
+    protected float hpRestore;
+    protected float manaRestore;
+    protected float hpIncrease;
+    protected float manaIncrease;
+    protected float damageIncrease;
+    protected float rangeIncrease;
+    protected float defenceIncrease;
+    protected float magDefenceIncrease;
+    protected float speedIncrease;
+
+}
