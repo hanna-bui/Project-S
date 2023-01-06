0797fb13408913a45db16bcc60477b986c01d5f4
isugakookie <hanna_buiii@hotmail.com>
2023-01-06 13:19:06 -0500

    added colliders, to detect player and move towardsit

--- Assets/Scripts/Movement/EnemyMovement.cs
+++ Assets/Scripts/Movement/EnemyMovement.cs
@@ -28,50 +28,89 @@ namespace Movement
 
         private List<Vector3> plus;
         private int index;
-        
         private List<Vector3> side;
-    
         private List<Vector3> random;
         private Vector3 newPos;
+
+        private Boolean detectingPlayer;
+        private Vector3 playerPos;
+        private List<Vector3> movePath;
 
         private void Start()
         {
             origin = transform.localPosition;
             gridObject = GameObject.Find("Grid");
             grid = gridObject.GetComponent<NewGrid>();
-
-            plus = new List<Vector3>();
-            side = new List<Vector3>();
+            
             random = null;
+            movePath = null;
             
             SetupPlus();
             
             SetupSide();
-        }
 
+            detectingPlayer = false;
+        }
+        
         // Update is called once per frame
         private void Update()
         {
-            switch (movementStyle)
+            if (!detectingPlayer)
             {
-                case MovementOptions.Plus:
-                    PlusMovement();
-                    break;
-                case MovementOptions.Side:
-                    SideMovement();
-                    break;
-                case MovementOptions.Random:
-                    RandomMovement();
-                    break;
-                case MovementOptions.None:
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
+                switch (movementStyle)
+                {
+                    case MovementOptions.Plus:
+                        PlusMovement();
+                        break;
+                    case MovementOptions.Side:
+                        SideMovement();
+                        break;
+                    case MovementOptions.Random:
+                        RandomMovement();
+                        break;
+                    case MovementOptions.None:
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+            else
+            {
+                MoveToPlayer();
             }
         }
 
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.gameObject.CompareTag("Player"))
+            {
+                Debug.Log("Hitting Player's Collider");
+                detectingPlayer = true;
+                var player = col.gameObject.transform;
+                if (movePath == null)
+                {
+                    playerPos = player.position;
+                    movePath = grid.CreatePath(transform, playerPos);
+                }
+            }
+        }
+        private void OnTriggerExit2D(Collider2D col)
+        {
+            if (col.gameObject.CompareTag("Player"))
+            {
+                Debug.Log("Leaving Player's Collider");
+                SetupPlus();
+                SetupSide();
+                detectingPlayer = false;
+            }
+        }
+
+        #region Movement Pattern
+        
         private void SetupPlus()
         {
+            plus = new List<Vector3>();
+            
             plus.Add(origin);
             
             var y = 20f;
@@ -108,7 +147,6 @@ namespace Movement
             }
             plus.Add(origin + new Vector3(x/scale, 0, 0));
         }
-
         private void PlusMovement()
         {
             var newPosition = plus[index];
@@ -129,6 +167,8 @@ namespace Movement
 
         private void SetupSide()
         {
+            side = new List<Vector3>();
+            
             side.Add(origin);
             var x = 20f;
             while (!grid.IsWalkable(transform.position + new Vector3(x, 0, 0)) && x > 0)
@@ -184,5 +224,34 @@ namespace Movement
                 transform.position = Vector3.MoveTowards(transform.position, targetLocation, speed * Time.deltaTime);
             }
         }
+        #endregion Movement Pattern
+
+        #region Detection of Player
+
+        private void MoveToPlayer()
+        {
+            if (movePath != null)
+            {
+                var targetLocation = movePath[0];
+                
+                if (transform.position.Equals(targetLocation)) movePath.RemoveAt(0);
+                
+                if (movePath.Count == 0)
+                {
+                    movePath = null;
+                }
+                transform.position = Vector3.MoveTowards(transform.position, targetLocation, speed * Time.deltaTime);
+            }
+            // detects if collision happened to a player
+            // if so, get the player's coordinates
+            // find the coordinates around the player, within the enemy's attacking radius
+            // choose the closest coordinates to move towards
+            // walk towards that coordinates
+
+            // enemy's attacking radius should be found in stats
+        }
+
+        #endregion Detection of Player
+        
     }
 }
