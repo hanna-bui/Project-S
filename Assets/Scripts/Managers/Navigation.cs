6bfed5b3a82494a90d0ad205905bde4425c9b865
Allyssa Poulin <40319407+allyssap@users.noreply.github.com>
2023-04-02 17:20:19 -0400

    added menus for setting up solo and multiplayer

---
+++ Assets/Scripts/Managers/Navigation.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Navigation : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("GameSetup");
+    }
+    public void HowTo()
+    {
+        ///load overlay canvas with simple instructions
+    }
+    public void Solo()
+    {
+        SceneManager.LoadScene("ChooseCharacter");
+    }
+    public void Multi()
+    {
+        SceneManager.LoadScene("MultiplayerNetwork");
+    }
+}
