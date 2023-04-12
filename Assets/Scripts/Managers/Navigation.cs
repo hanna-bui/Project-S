05016f32baa70325e95ce9cf94a16da45739324e
Allyssa Poulin <40319407+allyssap@users.noreply.github.com>
2023-04-12 16:49:57 -0400

    fixed multiplayer navigation to screens

--- Assets/Scripts/Managers/Navigation.cs
+++ Assets/Scripts/Managers/Navigation.cs
@@ -19,6 +19,6 @@ public class Navigation : MonoBehaviour
     }
     public void Multi()
     {
-        SceneManager.LoadScene("MultiplayerNetwork");
+        SceneManager.LoadScene("Starting");
     }
 }
